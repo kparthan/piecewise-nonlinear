1ae6ef6162bfd613d79bc3c56adbbebd79780e81
Parthan Kasarapu <pkas7@student.monash.edu>
2013-01-03 11:50:27 +1100

    Polynomial.cpp Polynomial.h:   Merged from 'experimental' branch -- working version of Polynomial solver!

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -14,7 +14,8 @@ Polynomial::Polynomial() : degree(-1)
  *  \param coefficients a reference to a vector<double>
  */
 Polynomial::Polynomial(const vector<double> &coefficients) : 
-                       coefficients(coefficients)
+                       coefficients(coefficients), 
+                       originalCoefficients(coefficients)
 {
   degree = coefficients.size() - 1;
   if (fabs(coefficients[degree]) <= ZERO) {
@@ -28,7 +29,8 @@ Polynomial::Polynomial(const vector<double> &coefficients) :
  *  \param source a reference to a Polynomial
  */
 Polynomial::Polynomial(const Polynomial &source) : degree(source.degree),
-            coefficients(source.coefficients), roots(source.roots)
+            coefficients(source.coefficients), roots(source.roots),
+            originalCoefficients(source.originalCoefficients)
 {}
 
 /*!
@@ -42,6 +44,7 @@ Polynomial Polynomial::operator=(const Polynomial &source)
     degree = source.degree;
     coefficients = source.coefficients;
     roots = source.roots;
+    originalCoefficients = source.originalCoefficients;
   }
   return *this;
 }
@@ -55,7 +58,7 @@ int Polynomial::getDegree()
 }
 
 /*!
- *  \brief This module returns the corresponding coefficient.
+ *  \brief This module returns the corresponding scaled coefficient.
  *  \param exponent an unsigned integer
  *  \return the coefficient
  */
@@ -83,7 +86,12 @@ void Polynomial::print()
 {
   cout << "Degree of the polynomial: " << degree << endl;
   if (coefficients.size() > 0) {
-    cout << "Coefficients: [ ";
+    cout << "Original coefficients: [ ";
+    for (int i=0; i<=degree; i++) {
+      cout << originalCoefficients[i] << " ";
+    }
+    cout << "]" << endl;
+    cout << "Scaled coefficients: [ ";
     for (int i=0; i<=degree; i++) {
       cout << coefficients[i] << " ";
     }
@@ -94,6 +102,47 @@ void Polynomial::print()
       cout << roots[i] << endl;
     }
   }
+}
+
+/*!
+ *  \brief This module computes the product of two complex numbers
+ *  \param c1 a reference to a complex<double>
+ *  \param c2 a reference to a complex<double>
+ *  \return the product as a complex number
+ */
+complex<double> complexProduct(const complex<double> &c1, 
+                               const complex<double> &c2)
+{
+  double real_part = c1.real() * c2.real() - c1.imag() * c2.imag();
+  double imaginary_part = c1.real() * c2.imag() + c2.real() * c1.imag();
+  return complex<double>(real_part,imaginary_part);
+}
+
+/*!
+ *  \brief This module computes the sum of two complex numbers
+ *  \param c1 a reference to a complex<double>
+ *  \param c2 a reference to a complex<double>
+ *  \return the sum as a compln number
+ */
+complex<double> complexSum(const complex<double> &c1, 
+                           const complex<double> &c2)
+{
+  double real_part = c1.real() + c2.real();
+  double imaginary_part = c1.imag() + c2.imag();
+  return complex<double>(real_part,imaginary_part);
+}
+
+/*!
+ *  \brief This module computes the product of a scalar with a complex number
+ *  \param scalar a double
+ *  \param c a reference to a complex<double>
+ *  \return the complex product
+ */
+complex<double> complexProduct(double scalar, complex<double> &c)
+{
+  double real_part = scalar * c.real();
+  double imaginary_part = scalar * c.imag();
+  return complex<double>(real_part,imaginary_part);
 }
 
 /*!
@@ -112,13 +161,31 @@ double Polynomial::value(double x)
 }
 
 /*!
+ *  \brief This module computes the function value at a given point in the
+ *  complex plane
+ *  \param number a complex<double>
+ *  \return the function value as a complex number
+ */
+complex<double> Polynomial::value(complex<double> number)
+{
+  complex<double> value(coefficients[0],0);
+  complex<double> exponent(1,0);
+  complex<double> temp;
+  for (int i=1; i<=degree; i++) {
+    exponent = complexProduct(exponent,number);  
+    temp = complexProduct(coefficients[i],exponent);
+    value = complexSum(value,temp);
+  }
+  return value;
+}
+
+/*!
  *  \brief This module computes the roots of the polynomial
  *  \return the roots of the polynomial
  */
 void Polynomial::findRoots()
 {
-  /* check if 0 is a root */
-  Polynomial p = refine(roots);
+  Polynomial p = preprocess();
   if (p.getDegree() == -1) {
     switch(degree) {
       case 1: /* LINEAR */
@@ -131,7 +198,6 @@ void Polynomial::findRoots()
 
       case 3: /* CUBIC */
         solveCubic();
-        //solveUsingBairstow();
         break;
 
       default:
@@ -153,11 +219,59 @@ void Polynomial::findRoots()
  *  \brief This module is a preprocessing routine which factors out a
  *  polynomial whose constant term is non-zero.
  *  \param roots a reference to a vector<complex<double>>
- *  \return a refined Polynomial
+ *  \return a preprocessed Polynomial
  */
-Polynomial Polynomial::refine(vector<complex<double>> &roots)
+Polynomial Polynomial::preprocess()
+{
+  normalize();
+
+  return removeTrivialRoots();
+}
+
+/*!
+ *  \brief This module finds the maximum absolute value in a list
+ *  \param list a reference to a vector<double>
+ *  \return the maximum absolute value
+ */
+double absoluteMaximum(vector<double> &list)
+{
+  double max = fabs(list[0]);
+  if (list.size() > 2) {
+    for (int i=1; i<list.size(); i++) {
+      if (fabs(list[i]) > max) {
+        max = fabs(list[i]);
+      }
+    }
+  }
+  return max;
+}
+
+/*!
+ *  \brief This module scales the coefficients of the polynomial so that
+ *  the absolute value of the maximum coefficient is 1. 
+
+ *  This is done for two reasons:-
+ *  1. Guards against the possibility of obtaining an arithmetic overflow 
+ *     during the calculation of the value of the polynomial.
+ *  2. Gets a more accurate approximation of a zero of the polynomial.
+ */
+void Polynomial::normalize()
+{
+  double max = absoluteMaximum(coefficients);
+  for (int i=0; i<=degree; i++) {
+    coefficients[i] /= max;
+  }
+}
+
+/*!
+ *  \brief This module removes any trivial roots (zeroes) and returns a 
+ *  polynomial with a non-zero constant term.
+ *  \return a Polynomial with no trivial roots
+ */
+Polynomial Polynomial::removeTrivialRoots()
 {
   int i = 0;
+  /* check if 0 is a root */
   while(fabs(coefficients[i]) < ZERO) {
     roots.push_back(complex<double>(0,0));
     i++;
@@ -181,7 +295,7 @@ Polynomial Polynomial::refine(vector<complex<double>> &roots)
 /*!
  *  \brief This module computes the root of a linear equation
  *  Solve: a x + b = 0
- *     Root x = -b / a
+ *  Root x = -b / a
  */
 void Polynomial::solveLinear()
 {
@@ -330,7 +444,7 @@ void Polynomial::solveCubic()
  */
 void Polynomial::solveUsingBairstow()
 {
-  Polynomial p = refine(roots);
+  Polynomial p = preprocess();
   if (p.getDegree() == -1) {
     bairstow(roots);
   } else if (p.getDegree() == 1) {
@@ -349,16 +463,21 @@ void Polynomial::solveUsingBairstow()
  *  roots of a polynomial.
  *
  *  Steps:-
- *  1. Assume initial values for r & s.
- *  2. Divide a polynomial f(x) by the quadratic : x^2 - r * x - s
- *      - f(x) = (x^2 - r * x - s) f_{n-2}(x) + R(x), where
- *        f_{n-2}(x) = b2 + b3*x + b4*x^2 + ... + b_{n-1}*x^{n-3} + bn*x^n
- *          and R(x) = b1*(x-r) + b0
- *     Since the quotient f_{n-2}(x) and the remainder R(x) are obtained by
+ *  1. Estimate initial values for r & s.
+ *  2. Divide a polynomial f(x) = a0 + a1*x + a2*x^2 + a3*x^3 + ... + an*x^n
+ *     by the quadratic expression: x^2 - r * x - s
+ *     Hence, f(x) = (x^2 - r * x - s) Q(x) + R(x), where
+ *            Q(x) = b2 + b3*x + b4*x^2 + ... + b_{n-1}*x^{n-3} + bn*x^{n-2}
+ *        and R(x) = b1*x + b0
+ *     Q(x) and R(x) are the quotient and remainder polynomials respectively.
+ *     Since the quotient Q(x) and the remainder R(x) are obtained by
  *     standard synthetic division, the coefficients bi (i=0,...,n) can be
  *     obtained by the following recurrence relation.
- *        bn = an, b_{n-1} = a_{n-1} + r*bn, and
- *        bi = ai + r*b_{i+1} + s*b_{i+2} for i=(n-2),(n-3),...,0
+ *        bn = an, 
+ *        b_{n-1} = a_{n-1} + r*bn, 
+ *        bi = ai + r*b_{i+1} + s*b_{i+2}, for i=(n-2),(n-3),...1, and
+ *        b0 = a0 + s*b2
+ *
  *  3. The goal is to determine r & s such that R(x) = 0. Apply strategy
  *     similar to Newton-Raphson's method. Expand b0, b1 using Taylor 
  *     series (consider only until first order) and equate them to zero:
@@ -368,27 +487,45 @@ void Polynomial::solveUsingBairstow()
  *    \frac{\partial b1}{\partial r}dr+\frac{\partial b1}{\partial s}ds=-b1
  *    \frac{\partial b0}{\partial r}dr+\frac{\partial b0}{\partial s}ds=-b0
  *     The above equations need to be solved for dr and ds. However, one 
- *     needs to estimate the partial derivatives. These are shown to follow
- *     the aforementioned recurrence relations replacing ai's with bi's and
- *     bi's with ci's. The ci's correspond to the respective partial
- *     derivatives. The recurrence relations are as follows:
- *        cn = bn, c_{n-1} = b_{n-1} + r*cn, and
- *        ci = bi + r*c_{i+1} + s*c_{i+2} for i=(n-2),(n-3),...,1, where
- *     \frac{\partial b0}{\partial r} = c1
- *     \frac{\partial b0}{\partial s} = \frac{\partial b1}{\partial r} = c2
- *     \frac{\partial b1}{\partial s} = c3
- *     The simultaneous linear equations to be solved will then be:
- *                                c2 dr + c3 ds = -b1  and
- *                                c1 dr + c2 ds = -b0
+ *     needs to estimate the partial derivatives. With a bit of arithmetic,
+ *     one can deduce the following recurrence relations connecting the
+ *     partial derivatives w.r.t. r and s with the coefficients bi's.
+ *     
+ *     Below: pr(i) => partial derivative of b_i w.r.t. r
+ *        and ps(i) => partial derivative of b_i w.r.t. s
+ *              pr(n)   = 0
+ *    sn      = pr(n-1) = bn
+ *    s_{n-1} = pr(n-2) = b_{n-1} + r*pr(n-1)
+ *    s_{i+1} = pr(i)   = b_{i+1} + r*pr(i+1) + s*pr(i+2), for i = (n-3),(n-4),...,1
+ *    s1      = pr(0)   = s*pr(2)
+ *
+ *                ps(n) = 0
+ *              ps(n-1) = 0
+ *    tn      = ps(n-2) = bn
+ *    t_{n-1} = ps(n-3) = b_{n-1} + r*ps(n-2)
+ *    t_{i+2} = ps(i)   = b_{i+2} + r*ps(i+1) + s*ps_{i+2}, for i = (n-4),(n-5),...,1
+ *    t2      = ps(0)   = b2 + s*ps(2)
+ *
+ *    \frac{\partial b1}{\partial r} = s2 = t2 + p*t3
+ *    \frac{\partial b1}{\partial s} = t3
+ *    \frac{\partial b0}{\partial r} = s1 = s * t3
+ *    \frac{\partial b0}{\partial s} = t2
+ *    The simultaneous linear equations to be solved will then be:
+ *                                pr(1) dr + ps(1) ds = -b1  and
+ *                                pr(0) dr + ps(0) ds = -b0
  *     Update r & s: r -> r + dr and s -> s + ds
- *  4. Repeat step (3) using new vlaues of r & s until relative errors of 
- *     r & s are below the precision required.
+ *
+ *  4. Repeat step (3) using new vlaues of r & s until convergence 
+ *     criterion is met. 
+ *
  *  5. At this stage, there exists a quadratic expression which is an 
  *     exact divisor of the given polynomial. The two roots of this 
  *     quadratic equation will be the roots of the polynomial as well.
+ *
  *  6. Apply the Bairstow method to the residual quotient polynomial if its
  *     degree is greater than 3. Else find its remaining roots using the
  *     customized solveCubic(), solveQuadratic() or solveLinear() methods.
+ *
  *  7. Append all the roots as they are computed to maintain them in the
  *     original Polynomial object.
  *
@@ -396,16 +533,12 @@ void Polynomial::solveUsingBairstow()
  */
 void Polynomial::bairstow(vector<complex<double>> &roots)
 {
-  double r,s,tol;
-  r = -coefficients[degree-1]/coefficients[degree];
-  if (fabs(r) < ZERO) {
-    r = 0.5;
-  }
-  s = -coefficients[degree-2]/coefficients[degree];
-  if (fabs(s) < ZERO) {
-    s = -0.5;
-  }
-  tol = 1e-6;
+  double tol = 1e-4;
+  double r = 0.5, s = -0.5;
+  /*array<double,2> initial_estimates = initializeRoots();
+  double r = initial_estimates[0];
+  double s = initial_estimates[1];*/
+  
   int count = 0;
 
   if (degree >= 3) {
@@ -414,10 +547,10 @@ void Polynomial::bairstow(vector<complex<double>> &roots)
       cout << "---------- Iteration " << ++count << " -----------" << endl;
       /* divide this polynomial by the quadratic: x^2 - r*x - s */
       b = divide(coefficients,r,s);
-      for (int i=0; i<b.size(); i++) {
-        /*cout << "a[" << i << "]: " << coefficients[i] << ";  b[" << i << "]: "
-        << b[i] << endl;*/
-      }
+      /*for (int i=0; i<b.size(); i++) {
+        cout << "a[" << i << "]: " << coefficients[i] << ";  b[" << i << "]: "
+        << b[i] << endl;
+      }*/
 
       /* compute the increments to r & s */
       array<double,2> increments = computeIncrements(b,r,s);
@@ -459,8 +592,91 @@ void Polynomial::bairstow(vector<complex<double>> &roots)
 }
 
 /*!
+ *  \brief This module computes the points at which the modulus of the 
+ *  polynomial is to be calculated.
+ *  \param r a double
+ *  \return the set of points used in the initial estimation process
+ */
+vector<complex<double>> Polynomial::predefinedPoints(double r)
+{
+  vector<complex<double>> points;
+  complex<double> p(0,0);
+  points.push_back(p);
+  p = complex<double>(r/2,0);
+  points.push_back(p);
+  p = complex<double>(r,0);
+  points.push_back(p);
+  p = complex<double>(0,fabs(r)/2);
+  points.push_back(p);
+  p = complex<double>(0,fabs(r));
+  points.push_back(p);
+  p = complex<double>(r,fabs(r));
+  points.push_back(p);
+  return points;
+}
+
+/*!
+ *  \brief This modulus computes the modulus of the polynomial at the
+ *  given points.
+ *  \param points a reference to a vector<complex<double>> 
+ *  \return the vector containing the modulus of the polynomial at the 
+ *  given points.
+ */
+vector<double> Polynomial::polynomialModulus(vector<complex<double>> &points)
+{
+  vector<double> modulus;
+  for (int i=0; i<points.size(); i++) {
+    complex<double> c = value(points[i]);
+    modulus.push_back(norm(c));
+  }
+  return modulus;
+}
+
+/*!
+ *  \brief This module uses the original polynomial modulus values at the 
+ *  given points and approximates them by a predefined bivariate polynomial
+ *  function: f(x,y) = a0 + a1*x + a2*y + a3*x*y + a4*x^2 + a5*y^2
+ *  Using the modulus values, the coefficients of the bivariate polynomial
+ *  are computed by solving a sustem of linear equations.
+ */
+vector<double> Polynomial::approximateModulus(vector<double> &modulus,
+              vector<complex<double>> &points)
+{
+  Matrix<double> A(6,6), B(6,1);
+  for (int i=0; i<6; i++) {
+    A[i][0] = 1;
+    A[i][1] = points[i].real();
+    A[i][2] = points[i].imag();
+    A[i][3] = points[i].real() * points[i].imag();
+    A[i][4] = points[i].real() * points[i].real();
+    A[i][5] = points[i].imag() * points[i].imag();
+  }
+}
+
+/*!
+ *  \brief This module computes the initial estimates to be used as roots.
+ *  \return initial estimates of the coefficients of the quadratic divisor
+ */
+array<double,2> Polynomial::initializeRoots()
+{
+  double product = fabs(coefficients[0]/coefficients[degree]);
+  double mean = pow(product,1/(double)degree);
+
+  /* compute the modulus of the polynomial value at select points */
+  vector<complex<double>> points = predefinedPoints(mean);
+  vector<double> modulus = polynomialModulus(points);
+  vector<double> modulus_approx = approximateModulus(modulus,points);
+  array<double,2> estimates;
+  return estimates;
+}
+
+/*!
  *  \brief This module computes the coefficients of the quotient using the
  *  recurrence relation
+ *        bn = an, 
+ *        b_{n-1} = a_{n-1} + r*bn, 
+ *        bi = ai + r*b_{i+1} + s*b_{i+2}, for i=(n-2),(n-3),...1, and
+ *        b0 = a0 + s*b2
  *  \param coefficients a reference to a vector<double>
  *  \param r a double
  *  \param s a double
@@ -472,10 +688,51 @@ vector<double> Polynomial::divide(const vector<double> &a, double r, double s)
   vector<double> b(n,0);
   b[n-1] = a[n-1];
   b[n-2] = a[n-2] + r * b[n-1];
-  for (int i=n-3; i>=0; i--) {
+  for (int i=n-3; i>=1; i--) {
     b[i] = a[i] + r * b[i+1] + s * b[i+2];
   }
+  b[0] = a[0] + s * b[2];
   return b;
+}
+
+/*!
+ *  \brief This module computes the partial derivatives of the coefficients of
+ *  the quotient polynomial w.r.t. the coefficients of the quadratic divisor
+ *  polynomial used in the Bairstow method.
+ *              pr(n)   = 0
+ *    sn      = pr(n-1) = bn
+ *    s_{n-1} = pr(n-2) = b_{n-1} + r*pr(n-1)
+ *    s_{i+1} = pr(i)   = b_{i+1} + r*pr(i+1) + s*pr(i+2), for i = (n-3),(n-4),...,1
+ *    s1      = pr(0)   = s*pr(2)
+ *
+ *                ps(n) = 0
+ *              ps(n-1) = 0
+ *    tn      = ps(n-2) = bn
+ *    t_{n-1} = ps(n-3) = b_{n-1} + r*ps(n-2)
+ *    t_{i+2} = ps(i)   = b_{i+2} + r*ps(i+1) + s*ps_{i+2}, for i = (n-4),(n-5),...,1
+ *    t2      = ps(0)   = b2 + s*ps(2)
+ *
+ *  \frac{\partial b1}{\partial r} = s2 = t2 + p*t3
+ *  \frac{\partial b1}{\partial s} = t3
+ *  \frac{\partial b0}{\partial r} = s1 = s * t3
+ *  \frac{\partial b0}{\partial s} = t2
+ *  \param b a reference to a vector<double>
+ *  \param r a double
+ *  \param s a double
+ *  \param flag a boolean
+ *  \return the partial derivatives
+ */
+vector<double> Polynomial::partialDerivatives(const vector<double> &b,
+                                              double r, double s)
+{
+  vector<double> t = divide(b,r,s);
+  t[2] = b[2] + s * t[4];
+  vector<double> derivatives(4,0);
+  derivatives[0] = t[2] + r * t[3];
+  derivatives[1] = t[3];
+  derivatives[2] = s * t[3];
+  derivatives[3] = t[2];
+  return derivatives;
 }
 
 /*!
@@ -491,21 +748,12 @@ array<double,2> Polynomial::computeIncrements(const vector<double> &b,
 {
   /* solve for the partial derivatives to be used in the 
      formulation of simultaneous linear equations */
-  vector<double> c = divide(b,r,s);
-  /*for (int i=1; i<=4; i++) {
-    cout << "c[" << i << "]: " << c[i] << endl;
-  }*/
-  Matrix<double> A(2,2),B(2,1),X(2,1) ;
-  A[0][0] = c[2]; A[0][1] = c[3];
-  A[1][0] = c[1]; A[1][1] = c[2];
-  B[0][0] = -b[1]; B[1][0] = -b[0];
-  //A.print(); B.print();
-  X = A.solveLinearSystem(B); 
-  //X.print();
+  vector<double> derivatives = partialDerivatives(b,r,s);
+  double d = derivatives[0] * derivatives[3] - derivatives[1] * derivatives[2];
+
   array<double,2> increments;
-  increments[0] = X[0][0];
-  increments[1] = X[1][0];
-  //A.print(); B.print(); X.print();
+  increments[0] = (b[0] * derivatives[1] - b[1] * derivatives[3]) / d;
+  increments[1] = (b[1] * derivatives[2] - b[0] * derivatives[0]) / d;
   return increments;
 }
 
@@ -533,7 +781,7 @@ array<double,2> Polynomial::relativeError(const array<double,2> &increments,
  */
 vector<complex<double>> Polynomial::solveGeneralEquation(double constant)
 {
-  vector<double> newCoefficients(coefficients);
+  vector<double> newCoefficients(originalCoefficients);
   newCoefficients[degree] = coefficients[degree] - constant;
   Polynomial newPolynomial(newCoefficients);
   return newPolynomial.getRoots();
