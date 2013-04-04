96e0d35ba52f52f213ddcbe86926bb5e3c5e8d3f
Parthan Kasarapu <pkas7@student.monash.edu>
2013-04-04 18:24:17 +1100

    Updated...

--- Structure.cpp
+++ Structure.cpp
@@ -114,14 +114,18 @@ void Structure::reconstruct(vector<vector<OptimalFit>> &optimalBezierFit,
 {
   switch(type) {
     case 0:
-      vector<array<int,3>> colours_generic = colourGeneric(segments.size());
-      reconstructGeneric(optimalBezierFit,segments,transformation,colours_generic);
+    {
+      vector<array<int,3>> colors_generic = colorGeneric(segments.size());
+      reconstructGeneric(optimalBezierFit,segments,transformation,colors_generic);
       break;
+    }
 
     case 1:
-      vector<array<double,3>> colours_protein = colourProtein(segments.size());
-      reconstructProtein(optimalBezierFit,segments,transformation,colours_protein);
+    {
+      vector<array<double,3>> colors_protein = colorProtein(segments.size());
+      reconstructProtein(optimalBezierFit,segments,transformation,colors_protein);
       break;
+    }
   }
 }
 
@@ -131,9 +135,12 @@ void Structure::reconstruct(vector<vector<OptimalFit>> &optimalBezierFit,
  *  \param optimalBezierFit a reference to a vector<vector<OptimalFit>>
  *  \param segments a reference to a vector<int>
  *  \param transformation a reference to a Matrix<double>
+ *  \param colors a reference to a vector<array<int,3>>
  */
 void Structure::reconstructGeneric(vector<vector<OptimalFit>> &optimalBezierFit,
-                            vector<int> &segments, Matrix<double> &transformation)
+                                   vector<int> &segments, 
+                                   Matrix<double> &transformation,
+                                   vector<array<int,3>> &colors)
 {
   int segment_start = 0;
   for(int i=1; i<segments.size(); i++) {
@@ -148,9 +155,12 @@ void Structure::reconstructGeneric(vector<vector<OptimalFit>> &optimalBezierFit,
  *  \param optimalBezierFit a reference to a vector<vector<OptimalFit>>
  *  \param segments a reference to a vector<int>
  *  \param transformation a reference to a Matrix<double>
+ *  \param colors a reference to a vector<array<int,3>>
  */
 void Structure::reconstructProtein(vector<vector<OptimalFit>> &optimalBezierFit,
-                            vector<int> &segments, Matrix<double> &transformation)
+                                   vector<int> &segments, 
+                                   Matrix<double> &transformation,
+                                   vector<array<double,3>> &colors)
 {
   protein->undoLastSelection();
   Matrix<double> inverse_transform = transformation.inverse();
@@ -176,27 +186,61 @@ void Structure::reconstructProtein(vector<vector<OptimalFit>> &optimalBezierFit,
     segment_start = segment_end;
   }
   protein->addChain(new_chain);
-  //vector<string> chain_ids = protein->getChainIdentifiers();
-  //for (int i=0; i<chain_ids)
   vector<Atom> atoms = protein->getAtoms();
   ofstream fw("new.pdb");
   for (int i=0; i<atoms.size(); i++) {
     fw << atoms[i].formatPDBLine() << endl;
   }
   fw.close();
-  createPymolScript();
+  createPymolScript(optimalBezierFit,segments,colors);
+  /*vector<string> chain_ids = protein->getChainIdentifiers();
+  for (int i=0; i<chain_ids.size(); i++) {
+    cout << chain_ids[i] << endl;
+  }
+  Chain chain = protein->getDefaultModel()["X"];
+  vector<string> res_ids = chain.getResidueIdentifiers();
+  for (int i=0; i<res_ids.size(); i++) {
+    cout << res_ids[i] << endl;
+  }*/
 }
 
 /*!
- *  \brief This fucntion generates the Pymol script to show the segments
+ *  \brief This function generates the Pymol script to show the segments
+ *  \param optimalBezierFit a reference to a vector<vector<OptimalFit>>
+ *  \param segments a reference to a vector<int>
+ *  \param colors a reference to a vector<array<double,3>>
  */
-void Structure::createPymolScript()
+void Structure::createPymolScript(vector<vector<OptimalFit>> &optimalBezierFit,
+                                  vector<int> &segments, 
+                                  vector<array<double,3>> &colors)
 {
+  Chain chain = protein->getDefaultModel()["X"];
+  vector<string> res_ids = chain.getResidueIdentifiers();
+
   ofstream script("segmentation.pml");
   script << "load new.pdb" << endl;
   script << "hide" << endl;
   script << "show cartoon" << endl;
-  for
+  int segment_start = 1;
+  for(int i=1; i<segments.size(); i++) {
+    // choose a color
+    script << "set_color c" << i << " = [" << colors[i-1][0]
+    << "," << colors[i-1][1] << "," << colors[i-1][2] << "]" << endl;
+    // select segment
+    int segment_end = segments[i] + 1;
+    script << "select seg" << i << ", (resi " << segment_start << "-" 
+    << segment_end;
+    OptimalFit fit = optimalBezierFit[segment_start-1][segment_end-1];
+    int numIntermediateControls = fit.getNumberOfControlPoints() - 2;
+    if (numIntermediateControls > 0) {
+      script << "," << res_ids[i-1];
+    }
+    script << ")" << endl;
+    // color the segment
+    script << "color c" << i << ", seg" << i << endl;  
+    segment_start = segment_end;
+  }
+  script.close();
 }
 
 //void Structure::reconstructProtein(vector<vector<OptimalFit>> &optimalBezierFit,
@@ -237,35 +281,37 @@ void Structure::createPymolScript()
 //}
 
 /*
- *  \brief This function generates the colours for the individual segments
+ *  \brief This function generates the colors for the individual segments
  *  of the generic 3D structure
  *  \param num_segments an integer
  *  \return the list of RGB values (0-255) corresponding to each segment 
  */
-vector<array<int,3>> Structure::colourGeneric(int num_segments)
+vector<array<int,3>> Structure::colorGeneric(int num_segments)
 {
   return randomRGB(num_segments);
 }
 
 /*
- *  \brief This function generates the colours for the individual segments
+ *  \brief This function generates the colors for the individual segments
  *  \param num_segments an integer
  *  \return the list of RGB values (0.0-1.0) corresponding to each segment 
  */
-vector<array<double,3>> Structure::colourProtein(int num_segments)
+vector<array<double,3>> Structure::colorProtein(int num_segments)
 {
   vector<array<int,3>> rgb_temp = randomRGB(num_segments);
-  vector<array<double,3>> rgb(rgb_temp);
+  vector<array<double,3>> rgb;
   for (int i=0; i<num_segments; i++) {
+    array<double,3> a;
     for (int j=0; j<3; j++) {
-      rgb[i][j] /= 255;
+      a[j] = ((double)rgb_temp[i][j]) / 255;
     }
+    rgb.push_back(a);
   }
   return rgb;
 }
 
 /*
- *  \brief This function generates the colours for the individual segments
+ *  \brief This function generates the colors for the individual segments
  *  \param num_segments an integer
  *  \return the list of RGB values (0-255) corresponding to each segment 
  */
--- Structure.h
+++ Structure.h
@@ -18,20 +18,24 @@ class Structure
 
     //! Reconstruct the generic structure back with the control points added
     void reconstructGeneric(vector<vector<OptimalFit>> &, vector<int> &, 
-                            Matrix<double> &);
+                            Matrix<double> &, vector<array<int,3>> &);
 
     //! Reconstruct the protein back with the control points added
     void reconstructProtein(vector<vector<OptimalFit>> &, vector<int> &, 
-                            Matrix<double> &);
+                            Matrix<double> &, vector<array<double,3>> &);
 
     //! Generates appropriate RGB indices for a generic structure
-    vector<array<int,3>> colourGeneric(int);
+    vector<array<int,3>> colorGeneric(int);
 
     //! Generates appropriate RGB indices for a protein structure
-    vector<array<double,3>> colourProtein(int);
+    vector<array<double,3>> colorProtein(int);
 
     //! Generate integral RGB indices
     vector<array<int,3>> randomRGB(int);
+
+    //! Generates the Pymol script for coloring the individual segments
+    void createPymolScript(vector<vector<OptimalFit>> &, vector<int> &,
+                           vector<array<double,3>> &);
 
   public:
     //! Constructor
@@ -58,7 +62,6 @@ class Structure
     //! Reconstruct the original structure back with the control points added
     void reconstruct(vector<vector<OptimalFit>> &, vector<int> &, 
                      Matrix<double> &);
-
 };
 
 #endif
